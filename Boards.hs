6ab1e47f64f1cf9ab7cfd0b68262a6879a67daa1
alexandremj <alexandremj@protonmail.com>
2021-12-07 13:38:45 -0300

    Fix previous commit

--- Boards.hs
+++ Boards.hs
@@ -1,17 +1,32 @@
-import qualified Cells.Cell
 
-module Boards (Board(Cell), createBoard, getLinha, getColuna) where
-    data Board = Board [[Cell]]
-        deriving Show
+module Boards (Board, Cell, getRow, getColumn) where
+    import Data.List
+    
+    -- the type Cell contains a 1 >= x >= N value, a boolean informing whether
+    -- its a black or white cell, and information on its row and column index
+    type Cell = (Int, Bool, Int, Int)
 
-    type Cell = Int Bool
+    -- in Straights a Board is just a NxN matrix of Cells
+    type Board = [[Cell]]
 
-    createBoard :: [[Cell]] -> Board
-    createBoard m = Board m
 
-    --  otimizacao
-    getLinha :: Board -> Int -> [Cell]
-    getLinha lineNumber b = b !! lineNumber
 
-    getColuna :: Board -> Int -> [Cell]
-    getColuna b n = map (!! n)
+    -- retrieves a row by its index number
+    -- @arguments:
+    --    Board b - the board containing the puzzle
+    --    Int n   - the number of the row to be returned
+    -- @returns:
+    --    a list of cells corresponding to the row on the puzzle
+    getRow :: Board -> Int -> [Cell]
+    getRow b n = b !! n
+
+
+
+    -- retrieves a column by its index number
+    -- @arguments:
+    --    Board b - the board containing the puzzle
+    --    Int n   - the number of the column to be returned
+    -- @returns:
+    --    a list of cells corresponding to the column on the puzzle             
+    getColumn :: Board -> Int -> [Cell]
+    getColumn b n = map (!! n) b
--- straights.hs
+++ straights.hs
@@ -1,12 +1,15 @@
 import Boards
 
 main = do
-    let cell = (createCell 1 False)
+    let cell = (1, False)
 
-    let puzzle = [[(createCell 0 False), (createCell 0 True), (createCell 0 True), (createCell 1 False), (createCell 0 False), (createCell 0 False)], [(createCell 0 False), (createCell 0 True), (createCell 0 True), (createCell 0 True), (createCell 5 True), (createCell 0 False)]]
+    let puzzle = [[(0, False, 0, 0), (0, True, 0, 1), (0, True, 0, 2), (1, False, 0, 3), (0, False, 0, 4), (0, False, 0, 5)], [(0, False, 1, 0), (0, True, 1, 1), (0, True, 1, 2), (0, True, 1, 3), (5, True, 1, 4), (0, False, 1, 5)]]
 
-    let line = (getLinha puzzle 0)
+    let row = (getRow puzzle 0)
+
+    let column = (getColumn puzzle 3)
 
     putStrLn (show cell)
     putStrLn (show puzzle)
-    putStrLn (show line)
+    putStrLn (show row)
+    putStrLn (show column)
