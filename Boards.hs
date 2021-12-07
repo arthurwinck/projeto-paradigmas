d5676075412d79075b840cd91afc8ead077a824f
alexandremj <alexandremj@protonmail.com>
2021-12-07 17:04:41 -0300

    Add verifications for single cells

--- Boards.hs
+++ Boards.hs
@@ -1,5 +1,7 @@
 
-module Boards (Board, Cell, getRow, getColumn) where
+module Boards (Board, Cell, getRow, getColumn, getValue, hasNoRepetitions,
+    respectsSequence, respectsColumn, respectsRow, isValidCell,
+    filterZeros) where
     import Data.List
     
     -- the type Cell contains a 1 >= x >= N value, a boolean informing whether
@@ -30,3 +32,124 @@ module Boards (Board, Cell, getRow, getColumn) where
     --    a list of cells corresponding to the column on the puzzle             
     getColumn :: Board -> Int -> [Cell]
     getColumn b n = map (!! n) b
+
+
+
+    -- retrieves the color from a cell
+    -- @arguments:
+    --     Cell (_, c, _, _) - the cell containing the color
+    -- @returns:
+    --     the color of the cell
+    getColor :: Cell -> Bool
+    getColor (_, c, _, _) = c
+
+
+
+    -- retrieves the value (apparent to the player) from a cell
+    -- @arguments:
+    --     Cell (v, _, _, _) - the cell containing the value
+    -- @returns:
+    --     the value of the cell
+    getValue :: Cell -> Int
+    getValue (v,_,_,_) = v
+
+
+
+    -- retrieves the values from a given row/column
+    -- @arguments:
+    --     [Cell] l - the list of cells
+    -- @returns:
+    --      a list containing the values of the cells
+    getValues :: [Cell] -> [Int]
+    getValues l = map getValue l
+
+
+
+
+    -- filters cells that contain zero from a given row/column
+    -- @arguments:
+    --     [Cell] l - the list of cells
+    -- @returns:
+    --      the list of values of the cells
+    filterZeros :: [Cell] -> [Cell]
+    filterZeros l = filter (\(x,_,_,_) -> x /= 0) l
+
+
+
+    -- retrieves the values (apparent to the player) from a list of cells
+    -- zeroed and black cells are filtered. useful for checking sequences
+    -- @arguments:
+    --     [Cell] l - the list of cells
+    -- @returns:
+    --      the list of values of the cells
+    filterZerosBlacks :: [Cell] -> [Cell]
+    filterZerosBlacks l = filterZeros (filter (\(_,c,_,_) -> c) (l))
+
+
+
+    -- checks if the list has repetitions
+    -- @arguments:
+    --    [Cell] l - list of Cells that needs to be checked
+    -- @returns:
+    --    True if the list has no repeated elements, False otherwise
+    hasNoRepetitions :: [Int] -> Bool
+    hasNoRepetitions l = length (nub l) == (length l)
+
+
+
+    -- TODO
+    -- checks if the given list follows a sequence, that is, if each element n
+    -- from the list has its predecessor and successor also on the list, with
+    -- the exception of the greater and the smaller elements
+    -- @arguments:
+    --     List l - the list to be checked
+    -- @returns:
+    --     True if the elements on the list are a sequence, False otherwise
+    -- we'll have to figure out this one later, tbh
+    respectsSequence :: [Cell] -> Bool
+    respectsSequence l = all (`elem` (getValues (filterZerosBlacks l))) [minimum (getValues (filterZerosBlacks l)) .. maximum (getValues(filterZerosBlacks l))]
+
+
+
+    -- checks whether there are any repeated numbers in this row
+    -- @arguments:
+    --     Board b - the board containing the cell
+    --     Cell (_, _, i, _) = takes the row index info from the cell
+    -- @returns: 
+    --     True if there are no repeated numbers, False otherwise
+    respectsRow :: Board -> Cell -> Bool
+    respectsRow b (_, _, i, _) = ((hasNoRepetitions (getValues (filterZeros (getRow b i))))
+                                && respectsSequence (getColumn b i))
+
+
+
+    -- checks whether there are any repeated numbers in this column
+    -- @arguments:
+    --     Board b - the board containing the cell
+    --     Cell (_, _, _, i) = takes the column index info from the cell
+    -- @returns: 
+    --     True if there are no repeated numbers, False otherwise
+    respectsColumn :: Board -> Cell -> Bool
+    respectsColumn b (_, _, _, i) = ((hasNoRepetitions (getValues (filterZeros (getColumn b i))))
+                                    && respectsSequence (getColumn b i))
+
+
+
+    -- checks if the cell respects the rules of the game
+    -- @arguments:
+    --     Board b - the Board that contains the cell
+    --     Cell c - the Cell to be verified
+    -- @returns:
+    --     True if the cell respects all the three rules, False otherwise
+    isValidCell :: Board -> Cell -> Bool
+    isValidCell b c = (respectsRow b c) && (respectsColumn b c)
+
+
+
+    -- checks if the current position of board is valid
+    -- @arguments:
+    --     Board b - the board to be checked
+    -- @returns:
+    --     True if the rules are followed for all the cells, False otherwise
+    -- isValidBoard :: Board -> Bool
+    -- isValidBoard b = elem False (map isValidCell (map all b))
